f7a7886d28874d5d95b9fdbcaf1f12873c22d661
square_1111 <tezuesh.varshney@gmail.com>
2018-03-10 21:51:45 +0530

    story begins

--- script/1.js
+++ script/1.js
@@ -1,13 +1,18 @@
 
 var myImage = document.querySelector('img') 
 
+
+var myhead = document.querySelector('h2')
+
 myImage.onclick = function(){
 	console.log("I clicked on myImage");
-	if(myImage.getAttribute('src') === 'image.png'){
-		myImage.setAttribute('src', 'Image2.png')
+	if(myImage.getAttribute('src') === 'image.gif'){
+		myImage.setAttribute('src', 'image2.png')
+		myhead.textContent('I am Pascal\'s triangle. I am your dad! :p')
 	}
 	else{
-		myImage.setAttribute('src','image.png')
+		myImage.setAttribute('src','image.gif')
+		myhead.textContent('Dont you dare call me that Pascal')
 	}
 } 
 
@@ -15,7 +20,7 @@ var myButton = document.getElementsByTagName("button");
 console.log(myButton);
 myButton.onclick = function(){
 	console.log("i clicked on button");
-	alert('achya. sahi hai');
+	alert('sahi hai');
 }
 
 function mul(num1,num2){
