c0daed15131c5714d25a30a26e974c13020e7a93
square_1111 <tezuesh.varshney@gmail.com>
2018-03-10 20:50:41 +0530

    hello

---
+++ script/1.js
@@ -0,0 +1,27 @@
+
+var myImage = document.querySelector('img') 
+
+myImage.onclick = function(){
+	console.log("I clicked on myImage");
+	if(myImage.getAttribute('src') === 'image.png'){
+		myImage.setAttribute('src', 'Image2.png')
+	}
+	else{
+		myImage.setAttribute('src','image.png')
+	}
+} 
+
+var myButton = document.getElementsByTagName("button");
+console.log(myButton);
+mybutton.onclick = function(){
+	console.log("i clicked on button");
+	alert('achya. sahi hai');
+}
+
+function mul(num1,num2){
+	var resul=num1*num2
+	return resul
+}
+num1=1
+num2=4
+console.log("hel" ,mul(num2,num1));
